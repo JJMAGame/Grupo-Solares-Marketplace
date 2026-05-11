2ae63c263f3983257ef8777693c816c85311ee64
José Jorge <154485319+JJMAGame@users.noreply.github.com>
2026-05-11 03:56:45 -0300

    Fazendo alguns testes para ver se consigo diminuir a linha

--- empresas.py
+++ empresas.py
@@ -1,1 +1,5 @@
 empresas = ["Empresa 1", "Empresa 2", "Empresa 3", "Empresa 4", "Empresa 5"]
+
+# TESTE TESTE E MAIS TESTE
+# empresas_existentes = print(
+ #   f"Empresas existentes no marketplace:\n {empresas[1]}\n {empresas[2]}\n {empresas[3]}\n {empresas[4]}")
